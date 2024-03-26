186a8a76c0ccdb8391e512480967c4ca26206ffe
Douglas Lima <douglas.lima.queiroz@gmail.com>
2024-03-26 09:51:33 -0300

    first commit

---
+++ src/test/java/com/pharmasystem/PharmaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
